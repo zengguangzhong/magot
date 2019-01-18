2b764d545f1dbeeef096cc13f3e167648b10d1ee
luoying <luoying@szzbmy.com>
2019-01-18 22:43:27 +0800

    1) webpack: inject __DEMOS__ and __ICONS__ variables 2) __DEMOS__ is all component demos 3) __ICONS__ is all icons of iconfont

--- scripts/webpack/dev.js
+++ scripts/webpack/dev.js
@@ -1,11 +1,33 @@
+import fs from 'fs';
 import path from 'path';
+import glob from 'glob';
 import webpack from 'webpack';
 import webpackMerge from 'webpack-merge';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import baseConfig from './base';
 
 const cwd = process.cwd();
+const src = path.resolve(cwd, 'src');
 const demo = path.resolve(cwd, 'demo');
+
+function getDemos() {
+  const demoFiles = glob.sync('**/demo.tsx', { cwd: src });
+  return demoFiles.map(file => {
+    return file.replace(/(components\/|\/demo.tsx)/g, '');
+  });
+}
+
+function getIcons() {
+  const icons = [];
+  const iconfontPath = path.join(src, 'iconfont/iconfont.less');
+  const iconfontData = fs.readFileSync(iconfontPath, 'utf8');
+  const iconPattern = /.mgt-icon-(.+?)::before/gi;
+  let match = null;
+  while ((match = iconPattern.exec(iconfontData))) {
+    icons.push(match[1]);
+  }
+  return icons;
+}
 
 export default webpackMerge(baseConfig, {
   mode: 'development',
@@ -26,6 +48,10 @@ export default webpackMerge(baseConfig, {
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
+    new webpack.DefinePlugin({
+      __DEMOS__: JSON.stringify(getDemos()),
+      __ICONS__: JSON.stringify(getIcons()),
+    }),
     new HtmlWebpackPlugin({
       template: path.resolve(cwd, 'demo/index.html'),
     }),
