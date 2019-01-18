9ab6cebae6033ec28a0c8c1de12270fd5ef218f6
luoying <luoying@szzbmy.com>
2019-01-18 22:45:53 +0800

    use __ICONS__ to render all icons for Icon demo

--- src/components/Icon/demo.tsx
+++ src/components/Icon/demo.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import Icon from './Icon';
 import { Link } from 'react-router-dom';
 
+const icons = __ICONS__ || [];
+
 function IconDemo() {
   return (
     <>
@@ -9,11 +11,20 @@ function IconDemo() {
         返回
       </Link>
       <div className="demo-box">
-        <Icon name="scan" />
-        <Icon name="search" />
-        <Icon name="download" />
-        <Icon name="loading" spin={true} />
-        <Icon name="chat" fontSize={32} style={{ color: '#1890ff' }} />
+        {icons.map(icon => {
+          return (
+            <Icon
+              key={icon}
+              name={icon}
+              fontSize={32}
+              style={{
+                color: '#bfbfbf',
+                marginRight: 30,
+                marginBottom: 30,
+              }}
+            />
+          );
+        })}
       </div>
     </>
   );
