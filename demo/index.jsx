58638a5ef368ff2aa70a193b32638b5d19897209
luoying <luoying@szzbmy.com>
2019-01-18 22:45:11 +0800

    use __DEMOS__ to render all component demo routes

--- demo/index.jsx
+++ demo/index.jsx
@@ -4,63 +4,14 @@ import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 
 import './index.less';
 
-const demoComponents = {
-  AutoComplete: React.lazy(() => import('../src/components/AutoComplete/demo')),
-  Avatar: React.lazy(() => import('../src/components/Avatar/demo')),
-  Badge: React.lazy(() => import('../src/components/Badge/demo')),
-  Breadcrumb: React.lazy(() => import('../src/components/Breadcrumb/demo')),
-  Button: React.lazy(() => import('../src/components/Button/demo')),
-  ButtonGroup: React.lazy(() => import('../src/components/ButtonGroup/demo')),
-  Calendar: React.lazy(() => import('../src/components/Calendar/demo')),
-  Card: React.lazy(() => import('../src/components/Card/demo')),
-  Carousel: React.lazy(() => import('../src/components/Carousel/demo')),
-  Checkbox: React.lazy(() => import('../src/components/Checkbox/demo')),
-  CheckboxGroup: React.lazy(() =>
-    import('../src/components/CheckboxGroup/demo')
-  ),
-  Collapse: React.lazy(() => import('../src/components/Collapse/demo')),
-  ColorPicker: React.lazy(() => import('../src/components/ColorPicker/demo')),
-  ContextMenu: React.lazy(() => import('../src/components/ContextMenu/demo')),
-  DatePicker: React.lazy(() => import('../src/components/DatePicker/demo')),
-  DateTimePicker: React.lazy(() =>
-    import('../src/components/DateTimePicker/demo')
-  ),
-  Drawer: React.lazy(() => import('../src/components/Drawer/demo')),
-  Dropdown: React.lazy(() => import('../src/components/Dropdown/demo')),
-  Empty: React.lazy(() => import('../src/components/Empty/demo')),
-  FileButton: React.lazy(() => import('../src/components/FileButton/demo')),
-  Form: React.lazy(() => import('../src/components/Form/demo')),
-  Gallery: React.lazy(() => import('../src/components/Gallery/demo')),
-  Icon: React.lazy(() => import('../src/components/Icon/demo')),
-  Image: React.lazy(() => import('../src/components/Image/demo')),
-  Input: React.lazy(() => import('../src/components/Input/demo')),
-  InputNumber: React.lazy(() => import('../src/components/InputNumber/demo')),
-  Layout: React.lazy(() => import('../src/components/Layout/demo')),
-  List: React.lazy(() => import('../src/components/List/demo')),
-  Loading: React.lazy(() => import('../src/components/Loading/demo')),
-  Menu: React.lazy(() => import('../src/components/Menu/demo')),
-  Modal: React.lazy(() => import('../src/components/Modal/demo')),
-  Notification: React.lazy(() => import('../src/components/Notification/demo')),
-  Pagination: React.lazy(() => import('../src/components/Pagination/demo')),
-  Popover: React.lazy(() => import('../src/components/Popover/demo')),
-  Qrcode: React.lazy(() => import('../src/components/Qrcode/demo')),
-  Radio: React.lazy(() => import('../src/components/Radio/demo')),
-  RadioGroup: React.lazy(() => import('../src/components/RadioGroup/demo')),
-  Rate: React.lazy(() => import('../src/components/Rate/demo')),
-  Search: React.lazy(() => import('../src/components/Search/demo')),
-  Select: React.lazy(() => import('../src/components/Select/demo')),
-  Shortcuts: React.lazy(() => import('../src/components/Shortcuts/demo')),
-  Slider: React.lazy(() => import('../src/components/Slider/demo')),
-  Switch: React.lazy(() => import('../src/components/Switch/demo')),
-  Table: React.lazy(() => import('../src/components/Table/demo')),
-  Tabs: React.lazy(() => import('../src/components/Tabs/demo')),
-  TimePicker: React.lazy(() => import('../src/components/TimePicker/demo')),
-  Toast: React.lazy(() => import('../src/components/Toast/demo')),
-  Tooltip: React.lazy(() => import('../src/components/Tooltip/demo')),
-  Tree: React.lazy(() => import('../src/components/Tree/demo')),
-  Uploader: React.lazy(() => import('../src/components/Uploader/demo')),
-  VideoPlayer: React.lazy(() => import('../src/components/VideoPlayer/demo')),
-};
+const demoComponents = {};
+if (__DEMOS__ && Array.isArray(__DEMOS__)) {
+  for (const demo of __DEMOS__) {
+    demoComponents[demo] = React.lazy(() => {
+      return import('../src/components/' + demo + '/demo.tsx');
+    });
+  }
+}
 
 function Home() {
   const links = [];
