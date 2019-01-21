f150c45d6aa7a5e65d5c9e328fffbe97cb285dc3
luoying <luoying@szzbmy.com>
2019-01-21 22:22:16 +0800

    implement the Loading component

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -1,16 +1,85 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 import './Loading.less';
 
+import Icon from '../Icon';
 import * as component from '../component';
 
-export interface LoadingProps extends component.ComponentBase {}
+export type Diretion = 'row' | 'column';
 
-const defaultProps: Partial<LoadingProps> = {};
+export interface LoadingProps
+  extends component.ComponentBase,
+    component.SizedComponent,
+    component.NestedComponent {
+  /**
+   * 是否是全局Loading，将覆盖在整个body之上
+   * @default false
+   */
+  global?: boolean;
+
+  /**
+   * 是否正在加载状态，只有在加载状态时，才渲染Loading组件
+   * @default true
+   */
+  loading?: boolean;
+
+  /**
+   * 加载提示文案
+   */
+  tip?: string;
+
+  /**
+   * 加载图标与提示文案的布局方向，可选值：row-按行布局，column-按列布局，默认`column`
+   * @default column
+   */
+  direction?: Diretion;
+}
+
+const defaultProps: Partial<LoadingProps> = {
+  ...component.getDefaultSizedProps(),
+  global: false,
+  loading: true,
+  direction: 'column',
+};
 
 function Loading(props: LoadingProps) {
-  const cls = component.getComponentClasses('loading', props);
-  return <div className={cls} />;
+  if (!props.loading) return null;
+  if (props.global) {
+    return <GlobalLoading {...props} />;
+  }
+  if (props.children) {
+    return <ContainerLoading {...props}>{props.children}</ContainerLoading>;
+  }
+  return <CommonLoading {...props} />;
+}
+
+function CommonLoading(props: LoadingProps) {
+  const type = 'loading';
+  const prefix = component.getComponentPrefix(type);
+  const cls = component.getComponentClasses(type, props, {
+    [`${prefix}-global`]: !!props.global,
+    [`${prefix}-row`]: props.direction === 'row',
+  });
+  return (
+    <div className={cls}>
+      <Icon name="loading" spin={true} />
+      {props.tip && <p className="tip">{props.tip}</p>}
+    </div>
+  );
+}
+
+function ContainerLoading(props: LoadingProps) {
+  return (
+    <div className="msg-loading-container">
+      <CommonLoading {...props} />
+      {props.children}
+    </div>
+  );
+}
+
+function GlobalLoading(props: LoadingProps) {
+  return ReactDOM.createPortal(<CommonLoading {...props} />, document.body);
 }
 
 Loading.defaultProps = defaultProps;
--- src/components/Loading/demo.tsx
+++ src/components/Loading/demo.tsx
@@ -1,15 +1,58 @@
-import React from 'react';
-import Loading from './Loading';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import Loading from './Loading';
+import Button from '../Button';
+import { useCountdown } from '../../hooks/countdown';
 
 function LoadingDemo() {
+  const [loading, setLoading] = useState(false);
+  const onChangeLoading = () => setLoading(true);
+
+  const time = useCountdown(5, loading);
+  if (time === 0 && loading) setLoading(false);
+
   return (
     <>
       <Link to="/" className="demo-goback">
         返回
       </Link>
       <div className="demo-box">
+        <Loading size="small" />
         <Loading />
+        <Loading size="large" />
+      </div>
+      <div className="demo-box">
+        <Loading tip="正在加载中..." size="small" />
+        <Loading tip="正在加载中..." />
+        <Loading tip="正在加载中..." size="large" />
+      </div>
+      <div className="demo-box">
+        <Loading tip="正在加载中..." direction="row" size="small" />
+        <Loading tip="正在加载中..." direction="row" />
+        <Loading tip="正在加载中..." direction="row" size="large" />
+      </div>
+      <div className="demo-box">
+        <Loading tip="正在加载中..." size="large">
+          <p>
+            React makes it painless to create interactive UIs. Design simple
+            views for each state in your application, and React will efficiently
+            update and render just the right components when your data changes.
+          </p>
+          <Button type="primary" href="https://reactjs.org/" target="_blank">
+            Click me for detail.
+          </Button>
+        </Loading>
+      </div>
+      <div className="demo-box">
+        <Button type="primary" onClick={onChangeLoading}>
+          点击加载
+        </Button>
+        <Loading
+          loading={loading}
+          global={true}
+          size="large"
+          tip={`正在加载中(${time}s)...`}
+        />
       </div>
     </>
   );
