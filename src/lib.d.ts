3e8a332cef4077f43c842c6838b354762a2a27bd
luoying <luoying@szzbmy.com>
2019-01-23 13:07:17 +0800

    add Size, Offset declarations

--- src/lib.d.ts
+++ src/lib.d.ts
@@ -2,3 +2,13 @@
 declare const __DEV__: boolean;
 declare const __DEMOS__: string[];
 declare const __ICONS__: string[];
+
+interface Size {
+  width: number;
+  height: number;
+}
+
+interface Offset {
+  left: number;
+  top: number;
+}
