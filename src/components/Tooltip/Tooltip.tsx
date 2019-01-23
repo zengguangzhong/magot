09b538dfa9c3d39ecf898b981a20af2ffb376c3c
luoying <luoying@szzbmy.com>
2019-01-23 13:10:34 +0800

    implement the Tooltip component

--- src/components/Tooltip/Tooltip.tsx
+++ src/components/Tooltip/Tooltip.tsx
@@ -1,16 +1,146 @@
-import React from 'react';
+import React, {
+  useState,
+  cloneElement,
+  Children,
+  ReactNode,
+  MouseEvent,
+  useRef,
+  useEffect,
+} from 'react';
+import ReactDOM from 'react-dom';
 
 import './Tooltip.less';
 
 import * as component from '../component';
+import * as node from '../../utils/node';
+import { getAdjustment, getAlignment, Alignment } from '../../utils/alignment';
 
-export interface TooltipProps extends component.ComponentBase {}
+export interface TooltipProps
+  extends component.ComponentBase,
+    component.NestedComponent {
+  /**
+   * 提示文案
+   */
+  title: string | ReactNode;
 
-const defaultProps: Partial<TooltipProps> = {};
+  /**
+   * 气泡位置，12个方位，
+   * 可选值：`left`,`top`,`right`,`bottom`,`topLeft`,`topRight`,`bottomLeft`,`bottomRight`,`leftTop`,`leftBottom`,`rightTop`,`rightBottom`，
+   * 默认值是`top`
+   * @default top
+   */
+  align?: Alignment;
+}
+
+interface WrappedComponentProps extends component.NestedComponent {
+  onMouseEnter: (e: MouseEvent<HTMLElement>) => void;
+  onMouseLeave: (e: MouseEvent<HTMLElement>) => void;
+}
+
+interface OverlayProps extends TooltipProps {
+  hidden?: boolean;
+  visible: boolean;
+  targetOffset: Offset;
+  targetSize: Size;
+}
+
+const transitionDuration = 200;
+const adjust = getAdjustment(10);
+const alignment = getAlignment(10);
+
+const defaultProps: Partial<TooltipProps> = {
+  align: 'top',
+};
 
 function Tooltip(props: TooltipProps) {
-  const cls = component.getComponentClasses('tooltip', props);
-  return <div className={cls} />;
+  if (!props.title || !props.children) return null;
+
+  const [visible, setVisible] = useState(false);
+  const [hidden, setHidden] = useState(false);
+  const [targetOffset, setTargetOffset] = useState({ left: 0, top: 0 });
+  const [targetSize, setTargetSize] = useState({ width: 0, height: 0 });
+
+  let leaveTimer = 0;
+
+  const onMouseEnter = (e: MouseEvent<HTMLElement>) => {
+    setHidden(false);
+    setVisible(true);
+    window.clearTimeout(leaveTimer);
+
+    const target = e.currentTarget;
+    const offet = node.offset(target, document.body);
+    if (offet) setTargetOffset(offet);
+    setTargetSize({ width: target.offsetWidth, height: target.offsetHeight });
+  };
+
+  const onMouseLeave = () => {
+    setHidden(true);
+    window.clearTimeout(leaveTimer);
+    leaveTimer = window.setTimeout(() => {
+      window.clearTimeout(leaveTimer);
+      setVisible(false);
+    }, transitionDuration);
+  };
+
+  return (
+    <>
+      <WrappedComponent onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
+        {props.children}
+      </WrappedComponent>
+      <Overlay
+        {...props}
+        hidden={hidden}
+        visible={visible}
+        targetOffset={targetOffset}
+        targetSize={targetSize}
+      />
+    </>
+  );
+}
+
+function WrappedComponent(props: WrappedComponentProps) {
+  const { children, ...otherProps } = props;
+  return cloneElement(Children.only(children), otherProps);
+}
+
+function Overlay(props: OverlayProps) {
+  if (!props.visible) return null;
+
+  const { targetOffset, targetSize } = props;
+  const overlayRef = useRef<HTMLDivElement>(null);
+  const [size, setSize] = useState({ width: 0, height: 0 });
+  const visibility = size.width > 0 && size.height > 0;
+  const target = { size: targetSize, offset: targetOffset };
+
+  useEffect(() => {
+    const node = overlayRef.current;
+    if (node && !visibility) {
+      setSize({ width: node.offsetWidth, height: node.offsetHeight });
+    }
+  });
+
+  const align = adjust[props.align || 'top'](size, target);
+  const pos = alignment[align](size, target);
+
+  const type = 'tooltip';
+  const arrow = getArrowAlignment(align);
+  const prefix = component.getComponentPrefix(type);
+  const cls = component.getComponentClasses(type, props, {
+    [`${prefix}-${arrow}`]: true,
+    show: visibility && !props.hidden,
+  });
+  return ReactDOM.createPortal(
+    <div ref={overlayRef} className={cls} style={{ ...pos }}>
+      {props.title}
+    </div>,
+    document.body
+  );
+}
+
+function getArrowAlignment(align: Alignment) {
+  const f = align[0];
+  const t = align.replace(/[a-z]/g, '');
+  return f + t.toLowerCase();
 }
 
 Tooltip.defaultProps = defaultProps;
--- src/components/Tooltip/demo.tsx
+++ src/components/Tooltip/demo.tsx
@@ -1,15 +1,97 @@
 import React from 'react';
-import Tooltip from './Tooltip';
 import { Link } from 'react-router-dom';
+import Tooltip from './Tooltip';
+import Button from '../Button';
 
 function TooltipDemo() {
+  const text = 'Tooltip text';
+  const style1 = {
+    width: 200,
+    marginBottom: 20,
+    paddingLeft: 60,
+    paddingRight: 60,
+    display: 'flex',
+    justifyContent: 'space-between',
+  };
+  const style2 = {
+    width: 320,
+    marginBottom: 20,
+    display: 'flex',
+    justifyContent: 'space-between',
+  };
   return (
     <>
       <Link to="/" className="demo-goback">
         返回
       </Link>
       <div className="demo-box">
-        <Tooltip />
+        <Tooltip title={text}>
+          <p style={{ display: 'inline-block' }}>
+            Hover on me to show tooltip.
+          </p>
+        </Tooltip>
+        <br />
+        <Tooltip title={text}>
+          <Button type="primary">Tooltip</Button>
+        </Tooltip>
+        <Tooltip
+          title={
+            <div>
+              {text}
+              <br />
+              {text}
+            </div>
+          }>
+          <Button type="primary">MultLine</Button>
+        </Tooltip>
+      </div>
+      <div className="demo-box">
+        <div style={style1}>
+          <Tooltip title={text} align="topLeft">
+            <Button>TL</Button>
+          </Tooltip>
+          <Tooltip title={text} align="top">
+            <Button>Top</Button>
+          </Tooltip>
+          <Tooltip title={text} align="topRight">
+            <Button>TR</Button>
+          </Tooltip>
+        </div>
+        <div style={style2}>
+          <Tooltip title={text} align="leftTop">
+            <Button>LT</Button>
+          </Tooltip>
+          <Tooltip title={text} align="rightTop">
+            <Button>RT</Button>
+          </Tooltip>
+        </div>
+        <div style={style2}>
+          <Tooltip title={text} align="left">
+            <Button>Left</Button>
+          </Tooltip>
+          <Tooltip title={text} align="right">
+            <Button>Right</Button>
+          </Tooltip>
+        </div>
+        <div style={style2}>
+          <Tooltip title={text} align="leftBottom">
+            <Button>LB</Button>
+          </Tooltip>
+          <Tooltip title={text} align="rightBottom">
+            <Button>RB</Button>
+          </Tooltip>
+        </div>
+        <div style={{ ...style1, marginBottom: 0 }}>
+          <Tooltip title={text} align="bottomLeft">
+            <Button>BL</Button>
+          </Tooltip>
+          <Tooltip title={text} align="bottom">
+            <Button>Bottom</Button>
+          </Tooltip>
+          <Tooltip title={text} align="bottomRight">
+            <Button>BR</Button>
+          </Tooltip>
+        </div>
       </div>
     </>
   );
