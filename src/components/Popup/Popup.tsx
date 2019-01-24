e4a04198b45402dcbb369392fb4141518bbb5046
luoying <luoying@szzbmy.com>
2019-01-24 19:38:30 +0800

    1) extract PopupOverlay child component 2) add `preventOut` optional property for Popup component

--- src/components/Popup/Popup.tsx
+++ src/components/Popup/Popup.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import cx from 'classnames';
 
 import './Popup.less';
 
+import PopupOverlay from './PopupOverlay';
 import * as component from '../component';
 import * as node from '../../utils/node';
-import { getAdjustment, getAlignment, Alignment } from '../../utils/alignment';
+import { Alignment } from '../../utils/alignment';
 
 export interface PopupProps
   extends component.BaseComponent,
     component.NestedComponent {
   /**
    * 对齐位置，12个方位，
-   * 可选值：`left`,`top`,`right`,`bottom`,`topLeft`,`topRight`,`bottomLeft`,`bottomRight`,`leftTop`,`leftBottom`,`rightTop`,`rightBottom`，
+   * 可选值：`left`,`top`,`right`,`bottom`,`topLeft`,`topRight`,
+   * `bottomLeft`,`bottomRight`,`leftTop`,`leftBottom`,`rightTop`,`rightBottom`，
    * 默认值是`top`
    * @default top
    */
@@ -23,37 +23,20 @@ export interface PopupProps
    * 弹出层组件，可以是任意需要被popup出来的组件
    */
   overlay: React.ReactElement<any>;
+
+  /**
+   * 当鼠标悬停在弹出层组件上时，是否阻止它隐藏
+   */
+  preventOut?: boolean;
 }
 
-interface WrappedComponentProps extends component.NestedComponent {
-  onMouseEnter: (e: React.MouseEvent<HTMLElement>) => void;
-  onMouseLeave: (e: React.MouseEvent<HTMLElement>) => void;
-}
+interface WrappedComponentProps
+  extends component.NestedComponent,
+    component.MouseMovableComponent<HTMLElement> {}
 
-interface OverlayProps extends PopupProps {
-  /**
-   * 开始离场(popdown)
-   */
-  leaving: boolean;
-
-  /**
-   * 是否可见
-   */
-  visible: boolean;
-
-  /**
-   * 目标组件的显示大小和绝对位置(相对于popup的容器来计算)
-   */
-  target: { size: Size; offset: Offset };
-
-  /**
-   * 离场动画结束的回调函数
-   */
-  onLeaved: () => void;
-}
-
-const adjust = getAdjustment(10);
-const alignment = getAlignment(10);
+type DispatchBoolean = React.Dispatch<React.SetStateAction<boolean>>;
+type DispatchSize = React.Dispatch<React.SetStateAction<Size>>;
+type DispatchOffset = React.Dispatch<React.SetStateAction<Offset>>;
 
 const defaultProps: Partial<PopupProps> = {
   align: 'top',
@@ -67,23 +50,27 @@ function Popup(props: PopupProps) {
   const [targetOffset, setTargetOffset] = React.useState({ left: 0, top: 0 });
   const [targetSize, setTargetSize] = React.useState({ width: 0, height: 0 });
 
-  const hanldeMouseEnter = (e: React.MouseEvent<HTMLElement>) => {
-    const target = e.currentTarget;
-    const offet = node.offset(target, document.body);
-    setLeaving(false);
-    setVisible(true);
-    offet && setTargetOffset(offet);
-    setTargetSize({ width: target.offsetWidth, height: target.offsetHeight });
-  };
+  const hanldeTargetEnter = getTargetEnterHandler(
+    setLeaving,
+    setVisible,
+    setTargetOffset,
+    setTargetSize
+  );
+  const handleTargetLeave = () => setLeaving(true);
 
-  const handleMouseLeave = () => setLeaving(true);
-  const handleLeaved = () => setVisible(false);
+  let handleOverlayEnter, handleOverlayLeave;
+  const handleOverlayLeaved = () => setVisible(false);
+
+  if (props.preventOut) {
+    handleOverlayEnter = () => setLeaving(false);
+    handleOverlayLeave = handleTargetLeave;
+  }
 
   return (
     <>
       <WrappedComponent
-        onMouseEnter={hanldeMouseEnter}
-        onMouseLeave={handleMouseLeave}>
+        onMouseEnter={hanldeTargetEnter}
+        onMouseLeave={handleTargetLeave}>
         {props.children}
       </WrappedComponent>
       <PopupOverlay
@@ -91,7 +78,9 @@ function Popup(props: PopupProps) {
         leaving={leaving}
         visible={visible}
         target={{ size: targetSize, offset: targetOffset }}
-        onLeaved={handleLeaved}
+        onMouseEnter={handleOverlayEnter}
+        onMouseLeave={handleOverlayLeave}
+        onLeaved={handleOverlayLeaved}
       />
     </>
   );
@@ -102,43 +91,20 @@ function WrappedComponent(props: WrappedComponentProps) {
   return React.cloneElement(React.Children.only(children), otherProps);
 }
 
-function PopupOverlay(props: OverlayProps) {
-  if (!props.visible) return null;
-
-  const { overlay, leaving, target, onLeaved } = props;
-  const overlayProps = overlay.props;
-
-  const [size, setSize] = React.useState({ width: 0, height: 0 });
-  const visibility = size.width > 0 && size.height > 0;
-
-  const popupRef = React.useRef<HTMLDivElement>(null);
-
-  React.useEffect(() => {
-    const node = popupRef.current;
-    if (node && !visibility) {
-      setSize({ width: node.offsetWidth, height: node.offsetHeight });
-    }
-    const handleTransitionEnd = () => leaving && onLeaved();
-    node && node.addEventListener('transitionend', handleTransitionEnd, false);
-    return () => {
-      node && node.removeEventListener('transitionend', handleTransitionEnd);
-    };
-  });
-
-  const align = adjust[props.align || 'top'](size, target);
-  const position = alignment[align](size, target);
-  const classes = component.getComponentClasses('popup', props, {
-    showed: visibility && !leaving,
-  });
-
-  return ReactDOM.createPortal(
-    <div className={classes} ref={popupRef} style={position}>
-      {React.cloneElement(overlay, {
-        className: cx(overlayProps.className, align.toLowerCase()),
-      })}
-    </div>,
-    document.body
-  );
+function getTargetEnterHandler(
+  setLeaving: DispatchBoolean,
+  setVisible: DispatchBoolean,
+  setTargetOffset: DispatchOffset,
+  setTargetSize: DispatchSize
+) {
+  return (e: React.MouseEvent<HTMLElement>) => {
+    const target = e.currentTarget;
+    const offet = node.offset(target, document.body);
+    setLeaving(false);
+    setVisible(true);
+    offet && setTargetOffset(offet);
+    setTargetSize({ width: target.offsetWidth, height: target.offsetHeight });
+  };
 }
 
 Popup.defaultProps = defaultProps;
---
+++ src/components/Popup/PopupOverlay.tsx
@@ -0,0 +1,79 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import cx from 'classnames';
+import { PopupProps } from './Popup';
+import * as component from '../component';
+import { getAdjustment, getAlignment } from '../../utils/alignment';
+
+const adjust = getAdjustment(10);
+const alignment = getAlignment(10);
+
+export interface OverlayProps
+  extends PopupProps,
+    component.MouseMovableComponent<HTMLElement> {
+  /**
+   * 开始离场(popdown)
+   */
+  leaving: boolean;
+
+  /**
+   * 是否可见
+   */
+  visible: boolean;
+
+  /**
+   * 目标组件的显示大小和绝对位置(相对于popup的容器来计算)
+   */
+  target: { size: Size; offset: Offset };
+
+  /**
+   * 离场动画结束的回调函数
+   */
+  onLeaved: () => void;
+}
+
+function PopupOverlay(props: OverlayProps) {
+  if (!props.visible) return null;
+
+  const { overlay, leaving, target, onLeaved } = props;
+  const overlayProps = overlay.props;
+
+  const [size, setSize] = React.useState({ width: 0, height: 0 });
+  const visibility = size.width > 0 && size.height > 0;
+
+  const popupRef = React.useRef<HTMLDivElement>(null);
+
+  React.useEffect(() => {
+    const node = popupRef.current;
+    if (node && !visibility) {
+      setSize({ width: node.offsetWidth, height: node.offsetHeight });
+    }
+    const handleTransitionEnd = () => leaving && onLeaved();
+    node && node.addEventListener('transitionend', handleTransitionEnd, false);
+    return () => {
+      node && node.removeEventListener('transitionend', handleTransitionEnd);
+    };
+  });
+
+  const align = adjust[props.align || 'top'](size, target);
+  const position = alignment[align](size, target);
+  const classes = component.getComponentClasses('popup', props, {
+    showed: visibility && !leaving,
+  });
+
+  return ReactDOM.createPortal(
+    <div
+      className={classes}
+      ref={popupRef}
+      style={position}
+      onMouseEnter={props.onMouseEnter}
+      onMouseLeave={props.onMouseLeave}>
+      {React.cloneElement(overlay, {
+        className: cx(overlayProps.className, align.toLowerCase()),
+      })}
+    </div>,
+    document.body
+  );
+}
+
+export default PopupOverlay;
