0feadb59d7d1042602d35c866c177a9a8078ba33
luoying <luoying@szzbmy.com>
2019-01-24 16:00:43 +0800

    1) use Popup component to wrap Tooltip component 2) use `.arrow()` less mixin

--- src/components/Tooltip/Tooltip.tsx
+++ src/components/Tooltip/Tooltip.tsx
@@ -1,19 +1,10 @@
-import React, {
-  useState,
-  cloneElement,
-  Children,
-  ReactNode,
-  MouseEvent,
-  useRef,
-  useEffect,
-} from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react';
 
 import './Tooltip.less';
 
+import Popup from '../Popup';
 import * as component from '../component';
-import * as node from '../../utils/node';
-import { getAdjustment, getAlignment, Alignment } from '../../utils/alignment';
+import { Alignment } from '../../utils/alignment';
 
 export interface TooltipProps
   extends component.ComponentBase,
@@ -21,7 +12,7 @@ export interface TooltipProps
   /**
    * 提示文案
    */
-  title: string | ReactNode;
+  title: string | React.ReactNode;
 
   /**
    * 气泡位置，12个方位，
@@ -32,117 +23,18 @@ export interface TooltipProps
   align?: Alignment;
 }
 
-interface WrappedComponentProps extends component.NestedComponent {
-  onMouseEnter: (e: MouseEvent<HTMLElement>) => void;
-  onMouseLeave: (e: MouseEvent<HTMLElement>) => void;
-}
-
-interface OverlayProps extends TooltipProps {
-  hidden?: boolean;
-  visible: boolean;
-  targetOffset: Offset;
-  targetSize: Size;
-}
-
-const transitionDuration = 200;
-const adjust = getAdjustment(10);
-const alignment = getAlignment(10);
-
-const defaultProps: Partial<TooltipProps> = {
-  align: 'top',
-};
-
 function Tooltip(props: TooltipProps) {
-  if (!props.title || !props.children) return null;
-
-  const [visible, setVisible] = useState(false);
-  const [hidden, setHidden] = useState(false);
-  const [targetOffset, setTargetOffset] = useState({ left: 0, top: 0 });
-  const [targetSize, setTargetSize] = useState({ width: 0, height: 0 });
-
-  let leaveTimer = 0;
-
-  const onMouseEnter = (e: MouseEvent<HTMLElement>) => {
-    setHidden(false);
-    setVisible(true);
-    window.clearTimeout(leaveTimer);
-
-    const target = e.currentTarget;
-    const offet = node.offset(target, document.body);
-    if (offet) setTargetOffset(offet);
-    setTargetSize({ width: target.offsetWidth, height: target.offsetHeight });
-  };
-
-  const onMouseLeave = () => {
-    setHidden(true);
-    window.clearTimeout(leaveTimer);
-    leaveTimer = window.setTimeout(() => {
-      window.clearTimeout(leaveTimer);
-      setVisible(false);
-    }, transitionDuration);
-  };
-
-  return (
-    <>
-      <WrappedComponent onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
-        {props.children}
-      </WrappedComponent>
-      <Overlay
-        {...props}
-        hidden={hidden}
-        visible={visible}
-        targetOffset={targetOffset}
-        targetSize={targetSize}
-      />
-    </>
-  );
-}
-
-function WrappedComponent(props: WrappedComponentProps) {
-  const { children, ...otherProps } = props;
-  return cloneElement(Children.only(children), otherProps);
-}
-
-function Overlay(props: OverlayProps) {
-  if (!props.visible) return null;
-
-  const { targetOffset, targetSize } = props;
-  const overlayRef = useRef<HTMLDivElement>(null);
-  const [size, setSize] = useState({ width: 0, height: 0 });
-  const visibility = size.width > 0 && size.height > 0;
-  const target = { size: targetSize, offset: targetOffset };
-
-  useEffect(() => {
-    const node = overlayRef.current;
-    if (node && !visibility) {
-      setSize({ width: node.offsetWidth, height: node.offsetHeight });
-    }
-  });
-
-  const align = adjust[props.align || 'top'](size, target);
-  const pos = alignment[align](size, target);
-
-  const type = 'tooltip';
-  const arrow = getArrowAlignment(align);
-  const prefix = component.getComponentPrefix(type);
-  const cls = component.getComponentClasses(type, props, {
-    [`${prefix}-${arrow}`]: true,
-    show: visibility && !props.hidden,
-  });
-  return ReactDOM.createPortal(
-    <div ref={overlayRef} className={cls} style={{ ...pos, ...props.style }}>
+  const cls = component.getComponentClasses('tooltip', props);
+  const tooltip = (
+    <div className={cls} style={{ ...props.style }}>
       {props.title}
-    </div>,
-    document.body
+    </div>
+  );
+  return (
+    <Popup align={props.align} overlay={tooltip}>
+      {props.children}
+    </Popup>
   );
 }
-
-function getArrowAlignment(align: Alignment) {
-  const f = align[0];
-  const t = align.replace(/[a-z]/g, '');
-  return f + t.toLowerCase();
-}
-
-Tooltip.defaultProps = defaultProps;
 
 export default Tooltip;
