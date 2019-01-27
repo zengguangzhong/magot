091a98f94aa23f3942b98b79939989e249935b57
luoying <luoying@szzbmy.com>
2019-01-27 18:47:22 +0800

    implement the ContextMenu component

--- src/components/ContextMenu/ContextMenu.tsx
+++ src/components/ContextMenu/ContextMenu.tsx
@@ -2,15 +2,31 @@ import React from 'react';
 
 import './ContextMenu.less';
 
+import { MenuProps } from '../Menu';
+import Dropdown from '../Dropdown';
 import * as component from '../component';
 
-export interface ContextMenuProps extends component.BaseComponent {}
+export interface ContextMenuProps
+  extends component.BaseComponent,
+    component.DisableComponent,
+    component.NestedComponent {
+  /**
+   * 下拉菜单组件(Menu)
+   */
+  menu: React.ReactElement<MenuProps>;
+}
 
-const defaultProps: Partial<ContextMenuProps> = {};
+const defaultProps: Partial<ContextMenuProps> = {
+  ...component.getDefaultDisabledProps(),
+};
 
 function ContextMenu(props: ContextMenuProps) {
   const cls = component.getComponentClasses('context-menu', props);
-  return <div className={cls} />;
+  return (
+    <Dropdown className={cls} {...props} trigger="contextMenu">
+      {props.children}
+    </Dropdown>
+  );
 }
 
 ContextMenu.defaultProps = defaultProps;
--- src/components/ContextMenu/demo.tsx
+++ src/components/ContextMenu/demo.tsx
@@ -1,15 +1,35 @@
 import React from 'react';
-import ContextMenu from './ContextMenu';
 import { Link } from 'react-router-dom';
+import ContextMenu from './ContextMenu';
+import Menu from '../Menu';
+import Button from '../Button';
 
 function ContextMenuDemo() {
+  const menu = (
+    <Menu>
+      <Menu.Item name="copy">Copy</Menu.Item>
+      <Menu.Item name="paste">Paste</Menu.Item>
+      <Menu.Divider />
+      <Menu.Item name="save" icon="publish">
+        Save
+      </Menu.Item>
+      <Menu.Item name="download" icon="download">
+        Download
+      </Menu.Item>
+    </Menu>
+  );
   return (
     <>
       <Link to="/" className="demo-goback">
         返回
       </Link>
       <div className="demo-box">
-        <ContextMenu />
+        <ContextMenu menu={menu}>
+          <Button type="primary">ContextMenu</Button>
+        </ContextMenu>
+        <ContextMenu menu={menu} disabled={true}>
+          <Button type="primary">ContextMenu(Disabled)</Button>
+        </ContextMenu>
       </div>
     </>
   );
