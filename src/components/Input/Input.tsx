7d5ea5e60769a31ed72e115b80ec12a2bb25f799
luoying <luoying@szzbmy.com>
2019-02-13 13:58:41 +0800

    Implement the Input component

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,16 +1,148 @@
 import React from 'react';
+import cx from 'classnames';
+
+import Icon from '../Icon';
+import Iconable from '../Icon/Iconable';
+import * as component from '../component';
 
 import './Input.less';
 
-import * as component from '../component';
+export interface InputProps
+  extends component.FormComponent<HTMLInputElement>,
+    component.SizedComponent,
+    component.IconableComponent,
+    component.KeyboardEventComponent<HTMLInputElement> {
+  /**
+   * 输入框占位符，默认空白
+   */
+  placeholder?: string;
 
-export interface InputProps extends component.BaseComponent {}
+  /**
+   * 最短字符数，默认无限制
+   */
+  minLength?: number;
 
-const defaultProps: Partial<InputProps> = {};
+  /**
+   * 最长字符数，默认无限制
+   */
+  maxLength?: number;
+
+  /**
+   * 是否是只读输入框
+   * @default false
+   */
+  readOnly?: boolean;
+
+  /**
+   * 可清除的输入框，即在输入框右侧显示清除图标
+   * @default false
+   */
+  clearable?: boolean;
+
+  /**
+   * 输入框内容发生变化时的回调函数
+   */
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+
+  /**
+   * 图标点击时的回调函数
+   */
+  onIconClick?: () => void;
+
+  /**
+   * 清除输入框内容后的回调函数
+   */
+  onClear?: () => void;
+}
+
+const defaultProps: Partial<InputProps> = {
+  ...component.getDefaultDisabledProps(),
+  ...component.getDefaultSizedProps(),
+  readOnly: false,
+  clearable: false,
+};
 
 function Input(props: InputProps) {
-  const cls = component.getComponentClasses('input', props);
-  return <div className={cls} />;
+  const {
+    className,
+    style,
+    size,
+    icon,
+    iconPosition,
+    iconSize,
+    clearable,
+    onIconClick,
+    onClear,
+    ...formProps
+  } = props;
+
+  const inputRef = React.useRef<HTMLInputElement>(null);
+  const [clearableState, setClearableState] = React.useState(false);
+
+  const updateClearableState = (value: string) => {
+    if (!clearable) return;
+    const state = value.length !== 0;
+    if (state !== clearableState) setClearableState(state);
+  };
+
+  React.useEffect(() => {
+    if (clearable && inputRef.current) {
+      const value = inputRef.current.value;
+      updateClearableState(value);
+    }
+  });
+
+  const type = 'input';
+  const prefix = component.getComponentPrefix(type);
+  const cls = component.getComponentClasses(type, props);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (clearable) updateClearableState(e.target.value);
+    formProps.onChange && formProps.onChange(e);
+  };
+
+  const handleClear = () => {
+    if (!inputRef.current || !inputRef.current.value) return;
+    const e = {
+      target: inputRef.current,
+      currentTarget: inputRef.current,
+    } as React.ChangeEvent<HTMLInputElement>;
+    inputRef.current.value = '';
+    if (clearableState) setClearableState(false);
+    onClear && onClear();
+    formProps.onChange && formProps.onChange(e);
+  };
+
+  return (
+    <div className={cls} style={style}>
+      <Iconable
+        className={prefix + '-icon'}
+        name={icon}
+        position={iconPosition || 'right'}
+        size={iconSize}
+        onClick={onIconClick}
+        style={{ cursor: onIconClick ? 'pointer' : undefined }}>
+        <input
+          {...formProps}
+          type="text"
+          ref={inputRef}
+          className={prefix + '-control'}
+          onChange={handleChange}
+        />
+      </Iconable>
+      {clearable && (
+        <Icon
+          name="close"
+          className={cx(
+            prefix + '-icon',
+            prefix + '-clear',
+            clearableState && 'show'
+          )}
+          onClick={handleClear}
+        />
+      )}
+    </div>
+  );
 }
 
 Input.defaultProps = defaultProps;
--- src/components/Input/demo.tsx
+++ src/components/Input/demo.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import Input from './Input';
 import { Link } from 'react-router-dom';
+import Input from './Input';
+import Toast from '../Toast';
 
 function InputDemo() {
   return (
@@ -9,7 +10,34 @@ function InputDemo() {
         返回
       </Link>
       <div className="demo-box">
-        <Input />
+        <Input
+          icon="search"
+          placeholder="This is a Input"
+          // tslint:disable-next-line
+          onChange={e => console.log(e.target.value)}
+          // tslint:disable-next-line
+          onIconClick={() => Toast.info('Clicked the Icon')}
+        />
+        <Input
+          icon="search"
+          iconPosition="left"
+          placeholder="This is a Input"
+        />
+        <Input placeholder="This is a Input" defaultValue="input content" />
+        <Input placeholder="This is a Input(disabled)" disabled={true} />
+        <Input
+          placeholder="Input with clear icon"
+          clearable={true}
+          // tslint:disable-next-line
+          onClear={() => Toast.info('Cleared')}
+          // tslint:disable-next-line
+          onChange={e => console.log(e.target.value)}
+        />
+      </div>
+      <div className="demo-box">
+        <Input icon="search" placeholder="This is a Input" size="small" />
+        <Input icon="search" placeholder="This is a Input" size="normal" />
+        <Input icon="search" placeholder="This is a Input" size="large" />
       </div>
     </>
   );
