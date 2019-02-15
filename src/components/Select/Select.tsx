77b953c2fcb593342081de4d8672a9d6fc4cec03
luoying <luoying@szzbmy.com>
2019-02-15 16:23:29 +0800

    Implement the Select component

--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -1,18 +1,195 @@
 import React from 'react';
 
-import './Select.less';
-
+import Menu, { MenuItemProps, MenuItemGroupProps } from '../Menu';
+import Popup from '../Popup';
+import Input from '../Input';
 import * as component from '../component';
 
-export interface SelectProps extends component.BaseComponent {}
+import './Select.less';
 
-const defaultProps: Partial<SelectProps> = {};
+export type SelectChildren =
+  | React.FunctionComponentElement<MenuItemProps>
+  | React.FunctionComponentElement<MenuItemGroupProps>
+  | Array<
+      | React.FunctionComponentElement<MenuItemProps>
+      | React.FunctionComponentElement<MenuItemGroupProps>
+    >;
+
+export interface SelectProps
+  extends component.FormComponent<HTMLInputElement>,
+    component.SizedComponent,
+    component.NestedComponent {
+  /**
+   * 指定选择框宽度
+   * @default 120
+   */
+  width?: number;
+
+  /**
+   * 选择框占位符
+   */
+  placeholder?: string;
+
+  /**
+   * 是否是多选选择框
+   * @default false
+   */
+  multiple?: boolean;
+
+  /**
+   * 可清除的选择框，即在选择框右侧显示清除图标
+   * @default false
+   */
+  clearable?: boolean;
+
+  /**
+   * 是否显示右侧的箭头符号
+   * @default true
+   */
+  showCaret?: boolean;
+
+  /**
+   * 选项列表
+   */
+  children?: SelectChildren;
+
+  /**
+   * 当前选中值发生变化后的回调函数
+   */
+  onChange?: (value: React.ReactText | undefined) => void;
+
+  /**
+   * 清除选择框内容后的回调函数
+   */
+  onClear?: () => void;
+}
+
+const defaultProps: Partial<SelectProps> = {
+  ...component.getDefaultDisabledProps(),
+  ...component.getDefaultSizedProps(),
+  width: 120,
+  multiple: false,
+  clearable: false,
+  showCaret: true,
+};
 
 function Select(props: SelectProps) {
-  const cls = component.getComponentClasses('select', props);
-  return <div className={cls} />;
+  const {
+    children,
+    className,
+    style,
+    width,
+    defaultValue,
+    value,
+    showCaret,
+    onChange,
+    onClear,
+    ...formProps
+  } = props;
+
+  const [menuVisible, setMenuVisible] = React.useState(false);
+  const [valueState, setValueState] = React.useState(defaultValue || value);
+
+  const type = 'select';
+  const prefix = component.getComponentPrefix(type);
+  const cls = component.getComponentClasses(
+    type,
+    { className },
+    { opened: menuVisible }
+  );
+
+  const handleOptionClick = (option: MenuItemProps) => {
+    if (option.value !== valueState) {
+      setValueState(option.value);
+      onChange && onChange(option.value);
+    }
+  };
+
+  const handlePopupOpen = () => setMenuVisible(true);
+  const handlePopupClose = () => setMenuVisible(false);
+
+  const handleClear = () => {
+    setValueState('');
+    onChange && onChange('');
+    onClear && onClear();
+  };
+
+  const trigger = (
+    <Input
+      {...formProps}
+      className={cls}
+      style={{ ...style, width: width }}
+      icon={showCaret ? 'caret' : undefined}
+      value={getLabelByValue(children, valueState)}
+      readOnly={true}
+      onClear={handleClear}
+    />
+  );
+
+  const overlay = (
+    <Menu
+      width={width}
+      className={prefix + '-menu'}
+      selectable={true}
+      selectedValues={valueState !== void 0 ? [valueState] : []}
+      onItemClick={handleOptionClick}>
+      {children}
+    </Menu>
+  );
+
+  return (
+    <Popup
+      className={prefix + '-popup'}
+      placement="bottomLeft"
+      trigger="click"
+      space={2}
+      overlay={overlay}
+      onOpen={handlePopupOpen}
+      onClose={handlePopupClose}>
+      {trigger}
+    </Popup>
+  );
+}
+
+function isReactTextChild(children?: React.ReactNode) {
+  if (children === void 0) return false;
+  return typeof children === 'string' || typeof children === 'number';
+}
+
+function getLabelByValue(children?: SelectChildren, value?: React.ReactText) {
+  const option = getOptionByValue(children, value);
+  if (!option) return '';
+
+  const optionProps = option.props;
+  if (optionProps.label !== void 0) return optionProps.label;
+  if (isReactTextChild(optionProps.children)) {
+    return optionProps.children as React.ReactText;
+  }
+  return optionProps.value;
+}
+
+function getOptionByValue(
+  children?: SelectChildren,
+  value?: React.ReactText
+): React.FunctionComponentElement<MenuItemProps> | null {
+  if (!children || value === void 0) return null;
+
+  for (const option of React.Children.toArray(children)) {
+    if ('items' in option.props) {
+      const children = option.props.children as SelectChildren;
+      const childOption = getOptionByValue(children, value);
+      if (childOption) return childOption;
+    } else {
+      const item = option as React.FunctionComponentElement<MenuItemProps>;
+      if (item.props.value === value) return item;
+    }
+  }
+
+  return null;
 }
 
 Select.defaultProps = defaultProps;
+Select.Option = Menu.Item;
+Select.OptionGroup = Menu.ItemGroup;
 
 export default Select;
--- src/components/Select/demo.tsx
+++ src/components/Select/demo.tsx
@@ -1,15 +1,134 @@
 import React from 'react';
-import Select from './Select';
 import { Link } from 'react-router-dom';
+import Select from './Select';
 
 function SelectDemo() {
+  const selectWidth = 180;
   return (
     <>
       <Link to="/" className="demo-goback">
         返回
       </Link>
       <div className="demo-box">
-        <Select />
+        <Select
+          width={selectWidth}
+          placeholder="Label Options"
+          clearable={true}
+          onChange={console.log}>
+          <Select.Option value="option1" label="Option1" />
+          <Select.Option value="option2" label="Option2" />
+          <Select.Option value="option3" label="Option3" />
+          <Select.Option value="option4" label="Option4" disabled={true} />
+        </Select>
+        <Select
+          width={selectWidth}
+          placeholder="Children Options"
+          onChange={console.log}>
+          <Select.Option value="option1" icon="home">
+            Option1
+          </Select.Option>
+          <Select.Option value="option2" icon="user">
+            Option2
+          </Select.Option>
+          <Select.Option value="option3" icon="store">
+            Option3
+          </Select.Option>
+          <Select.Option value="option4" icon="setting" disabled={true}>
+            Option4
+          </Select.Option>
+        </Select>
+        <Select
+          width={selectWidth}
+          disabled={true}
+          placeholder="Disabled Select">
+          <Select.Option value="option1" label="Option1" />
+          <Select.Option value="option2" label="Option2" />
+          <Select.Option value="option3" label="Option3" />
+          <Select.Option value="option4" label="Option4" disabled={true} />
+        </Select>
+        <Select
+          width={selectWidth}
+          placeholder="Group Select"
+          defaultValue="option1.1"
+          onChange={console.log}>
+          <Select.OptionGroup title="Group 1">
+            <Select.Option value="option1.1">Group Item 1.1</Select.Option>
+            <Select.Option value="option1.2">Group Item 1.2</Select.Option>
+            <Select.Option value="option1.3">Group Item 1.3</Select.Option>
+            <Select.Option value="option1.4" disabled={true}>
+              Group Item 1.4
+            </Select.Option>
+          </Select.OptionGroup>
+          <Select.OptionGroup title="Group 2">
+            <Select.Option value="option2.1">Group Item 2.1</Select.Option>
+            <Select.Option value="option2.2">Group Item 2.2</Select.Option>
+            <Select.Option value="option2.3">Group Item 2.3</Select.Option>
+            <Select.Option value="option2.4">Group Item 2.4</Select.Option>
+          </Select.OptionGroup>
+        </Select>
+      </div>
+      <div className="demo-box">
+        <Select
+          width={selectWidth}
+          defaultValue="option1"
+          size="small"
+          onChange={console.log}>
+          <Select.Option value="option1" label="Option1" />
+          <Select.Option value="option2" label="Option2" />
+          <Select.Option value="option3" label="Option3" />
+          <Select.Option value="option4" label="Option4" disabled={true} />
+        </Select>
+        <Select
+          width={selectWidth}
+          defaultValue="option1"
+          size="normal"
+          onChange={console.log}>
+          <Select.Option value="option1" label="Option1" />
+          <Select.Option value="option2" label="Option2" />
+          <Select.Option value="option3" label="Option3" />
+          <Select.Option value="option4" label="Option4" disabled={true} />
+        </Select>
+        <Select
+          width={selectWidth}
+          defaultValue="option1"
+          size="large"
+          onChange={console.log}>
+          <Select.Option value="option1" label="Option1" />
+          <Select.Option value="option2" label="Option2" />
+          <Select.Option value="option3" label="Option3" />
+          <Select.Option value="option4" label="Option4" disabled={true} />
+        </Select>
+      </div>
+      <div className="demo-box">
+        <Select
+          width={selectWidth}
+          defaultValue="menu_item_1"
+          onChange={console.log}>
+          <Select.Option value="menu_item_1" label="Menu Item 1">
+            <span style={{ float: 'left' }}>Menu Item 1</span>
+            <span style={{ float: 'right', color: '#d9d9d9', fontSize: 12 }}>
+              No.1
+            </span>
+          </Select.Option>
+          <Select.Option value="menu_item_2" label="Menu Item 2">
+            <span style={{ float: 'left' }}>Menu Item 2</span>
+            <span style={{ float: 'right', color: '#d9d9d9', fontSize: 12 }}>
+              No.2
+            </span>
+          </Select.Option>
+          <Select.Option value="menu_item_3" label="Menu Item 3">
+            <span style={{ float: 'left' }}>Menu Item 3</span>
+            <span style={{ float: 'right', color: '#d9d9d9', fontSize: 12 }}>
+              No.3
+            </span>
+          </Select.Option>
+          <Select.Option value="menu_item_4" label="Menu Item 4">
+            <span style={{ float: 'left' }}>Menu Item 4</span>
+            <span style={{ float: 'right', color: '#d9d9d9', fontSize: 12 }}>
+              No.4
+            </span>
+          </Select.Option>
+        </Select>
       </div>
     </>
   );
