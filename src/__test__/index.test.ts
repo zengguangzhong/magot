9d9a416c4a01379464880a36efe52d5f61a5b4f0
luoying <luoying@szzbmy.com>
2019-03-18 18:11:43 +0800

    Update the root test case

--- src/__test__/index.test.ts
+++ src/__test__/index.test.ts
@@ -1,6 +1,6 @@
 import * as components from '..';
 
-describe('magot component files', () => {
+describe('component files', () => {
   it('should export components correctly', () => {
     expect(Object.keys(components)).toMatchSnapshot();
   });
