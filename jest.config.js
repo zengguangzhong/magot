a7ef8a0f2a23a7f7d47c4bb225fb4b848c5458fd
luoying <luoying@szzbmy.com>
2019-03-18 17:08:46 +0800

    Update testMatch config of jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -135,10 +135,10 @@ module.exports = {
   // testLocationInResults: false,
 
   // The glob patterns Jest uses to detect test files
-  // testMatch: [
-  //   "**/__tests__/**/*.[jt]s?(x)",
-  //   "**/?(*.)+(spec|test).[tj]s?(x)"
-  // ],
+  testMatch: [
+    '<rootDir>/src/**/__test__/**/*.[jt]s?(x)',
+    '<rootDir>/src/**/?(*.)+(spec|test).[tj]s?(x)',
+  ],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
   // testPathIgnorePatterns: [
