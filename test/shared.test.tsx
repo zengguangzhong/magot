5c6b5edded3a572aa9d4e22fb722b688378d7f6b
luoying <luoying@szzbmy.com>
2019-03-20 14:25:28 +0800

    Change `injectTestSuites` function of the shared test

--- test/shared.test.tsx
+++ test/shared.test.tsx
@@ -4,27 +4,26 @@ import { render, shallow } from 'enzyme';
 
 export function injectTestSuites(
   Component: React.ComponentType<any>,
-  style: React.CSSProperties
+  style?: React.CSSProperties | null,
+  skipClickEvent?: boolean
 ) {
   it('should renders custom className', () => {
-    const wrapper = render(
-      <Component className="jest-test-class">Button</Component>
-    );
+    const wrapper = render(<Component className="jest-test-class" />);
     expect(wrapper).toMatchSnapshot();
     expect(wrapper.hasClass('jest-test-class')).toEqual(true);
   });
 
   it('should renders custom styles', () => {
-    const wrapper = render(<Component style={style}>My Component</Component>);
+    const wrapper = render(<Component style={style} />);
     expect(wrapper).toMatchSnapshot();
   });
 
-  it('should simulates click events', () => {
-    const handleClick = sinon.spy();
-    const wrapper = shallow(
-      <Component onClick={handleClick}>Click me</Component>
-    );
-    wrapper.simulate('click');
-    expect(handleClick).toHaveProperty('callCount', 1);
-  });
+  if (!skipClickEvent) {
+    it('should simulates click events', () => {
+      const handleClick = sinon.spy();
+      const wrapper = shallow(<Component onClick={handleClick} />);
+      wrapper.simulate('click');
+      expect(handleClick).toHaveProperty('callCount', 1);
+    });
+  }
 }
