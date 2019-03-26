b747a8db7ba6a0341583987b6e60c4b24603c0a2
luoying <luoying@szzbmy.com>
2019-03-26 16:02:20 +0800

    Remove unused files

--- scripts/less/find.js
+++
@@ -1,9 +0,0 @@
-// find all less files
-
-import glob from 'glob';
-
-const pattern = '**/*.less';
-
-export default function find(src) {
-  return glob.sync(pattern, { cwd: src, absolute: true });
-}
