36f1ab896c31b4c8d121fcd8c1e8c4411f8dcf39
luoying <luoying@szzbmy.com>
2019-05-20 11:04:51 +0800

    Update doczrc.js

--- doczrc.js
+++ doczrc.js
@@ -11,66 +11,59 @@ export default {
   codeSandbox: false,
   themeConfig: {
     // mode: 'dark',
-    styles: getThemeStyles(),
+    styles: {
+      body: {
+        fontFamily:
+          "'PingFang SC', 'Microsoft YaHei', 'Helvetica Neue', Helvetica, Arial, sans-serif",
+        fontSize: 14,
+      },
+      h1: {
+        fontFamily: 'none',
+        fontSize: 30,
+      },
+      h2: {
+        fontFamily: 'none',
+        fontSize: 24,
+      },
+      h3: {
+        fontSize: 18,
+      },
+      h4: {
+        fontSize: 16,
+      },
+      h5: {
+        fontSize: 14,
+      },
+      h6: {
+        fontSize: 14,
+      },
+      paragraph: {
+        fontSize: 14,
+      },
+      table: {
+        fontFamily: 'none',
+        fontSize: 12,
+      },
+      pre: {
+        fontSize: 12,
+      },
+      blockquote: {
+        fontSize: 14,
+      },
+    },
   },
-  plugins: getPlugins(),
-  menu: getMenuList(),
-};
-
-function getMenuList() {
-  return [
+  plugins: [
+    css({
+      /* stylelint-disable */
+      preprocessor: 'less',
+    }),
+  ],
+  menu: [
     'Introduction',
     'Components',
     {
       name: 'Github',
       href: 'https://github.com/billjs/magot',
     },
-  ];
-}
-
-function getPlugins() {
-  return [css({ preprocessor: 'less' })];
-}
-
-function getThemeStyles() {
-  return {
-    body: {
-      fontFamily:
-        "'PingFang SC', 'Microsoft YaHei', 'Helvetica Neue', Helvetica, Arial, sans-serif",
-      fontSize: 14,
-    },
-    h1: {
-      fontFamily: 'none',
-      fontSize: 30,
-    },
-    h2: {
-      fontFamily: 'none',
-      fontSize: 24,
-    },
-    h3: {
-      fontSize: 18,
-    },
-    h4: {
-      fontSize: 16,
-    },
-    h5: {
-      fontSize: 14,
-    },
-    h6: {
-      fontSize: 14,
-    },
-    paragraph: {
-      fontSize: 14,
-    },
-    table: {
-      fontFamily: 'none',
-      fontSize: 12,
-    },
-    pre: {
-      fontSize: 12,
-    },
-    blockquote: {
-      fontSize: 14,
-    },
-  };
-}
+  ],
+};
