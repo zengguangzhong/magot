2219b1984ec00ae595b3a54013dfa66c70d10c20
luoying <luoying@szzbmy.com>
2019-05-27 11:34:28 +0800

    Add `getShallowTextSize` node utility function

--- src/utils/node.ts
+++ src/utils/node.ts
@@ -42,3 +42,33 @@ export function isEditableNode(node: Node) {
   const editable = node.contentEditable;
   return inputableNodes.includes(node.nodeName) || editable === 'true';
 }
+
+/**
+ * 通过影子渲染的方式获得文本的渲染后大小
+ *
+ * @export
+ * @param {string} text 文本内容
+ * @param {(Node | null)} [parent] 渲染到的父级容器节点
+ * @param {CSSStyleDeclaration} [styles] 文本的样式
+ * @returns {{ width: number, height: number }}
+ */
+export function getShallowTextSize(
+  text: string,
+  parent?: Node | null,
+  styles?: CSSStyleDeclaration
+) {
+  const container = parent || document.body;
+  const node = document.createElement('span');
+  node.textContent = text;
+  node.style.visibility = 'hidden';
+  if (styles) {
+    for (const key in styles) {
+      node.style[key] = styles[key];
+    }
+  }
+  container.appendChild(node);
+  const width = node.offsetWidth;
+  const height = node.offsetHeight;
+  container.removeChild(node);
+  return { width, height };
+}
